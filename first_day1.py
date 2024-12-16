9e9dfe274b000aa72e2b3aa2ac1f1f27d5e51d42
Sufyan <muhammadsufyan11011@gmail.com>
2024-12-16 11:44:05 +0500

    python file is add

---
+++ first_day1.py
@@ -0,0 +1,107 @@
+# name="shardara"
+# age=43
+# age2=age
+# print("my name is :",name,"and my age is :",age2)
+
+# print(age2)
+
+# a=3
+# b=43
+# sum=a-b
+# print(sum)
+# arithemethic operator 
+# a=6
+# b=6
+# code=a/b
+# need=a%b
+# summ=a+b
+# dog=a-b
+# cat=a*b
+# animal=a**b
+# print(cat)
+# print(animal)
+# print(summ)
+# print(dog)
+# print(code)
+# print(need)
+# Relation opreator or commparision oprator
+
+# a=10
+# b=20
+# code=b<=a 
+# anit=b>=a
+# apple=a != b
+# oring=a==b
+# print(oring)
+# print(apple)
+# print(code)
+# print(anit)
+
+# Assignment opreator
+
+# a=20
+# a**=20
+# print(a)
+
+# logical oprator
+# val1=True
+# val2=True
+# print("and oprator:",val1 and val2)
+# print("or  oprator:",val1 or val2)
+
+
+# Type conversion and the type of casting 
+# a=float(33)
+# b=int("43")
+# dog=a+b
+# print(dog)
+# print(type(a))
+
+
+# Input in python 
+# name=input("Enter your name:")
+# print("Welcome",name)
+# int("44")
+# val=int(input("enter some value:"))
+# print(type(val),val)
+
+
+
+# float("3")
+# int("6")
+# i=int(input("Enter some value:"))
+# j=float(input("Enter any value"))
+# sum=i+j
+# print(sum)
+# print(i)
+
+
+# print(j)
+
+
+
+print("My name is ali and my father is shabbir ahmad ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
